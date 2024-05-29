e2174fd5b0e74a912d73e001650a502dd45fab98
erick <ericksonoliva@gmail.com>
2024-05-28 18:37:01 -0600

    eliminar en casacada implementado

--- Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -39,12 +39,12 @@ namespace proyectoIngSoftware.Areas.Identity.Pages.Account
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{userId}'.");
+                return NotFound($"No se encontro al usuario '{userId}'.");
             }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Gracias por confirmar tu email, ya puedes iniciar sesion" : "Ocurrio un error confirmando el correo.";
             return Page();
         }
     }
--- Models/TransmetroContext.cs
+++ Models/TransmetroContext.cs
@@ -17,18 +17,6 @@ public partial class TransmetroContext : DbContext
 
     public virtual DbSet<Accesos> Accesos { get; set; }
 
-    public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
-
-    public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
-
-    public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
-
-    public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
-
-    public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
-
-    public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
-
     public virtual DbSet<Buses> Buses { get; set; }
 
     public virtual DbSet<Empleados> Empleados { get; set; }
@@ -54,94 +42,75 @@ public partial class TransmetroContext : DbContext
     {
         modelBuilder.Entity<Accesos>(entity =>
         {
-            entity.HasKey(e => e.IdAcceso).HasName("PK__accesos__20ABE01384D23617");
+            entity.HasKey(e => e.IdAcceso).HasName("PK__accesos__20ABE0132FCFB35C");
 
             entity.Property(e => e.Descripcion).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdEstacion).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdEstacionNavigation).WithMany(p => p.Accesos).HasConstraintName("FK_accesos_estaciones");
-        });
-
-        modelBuilder.Entity<AspNetRoles>(entity =>
-        {
-            entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
-                .IsUnique()
-                .HasFilter("([NormalizedName] IS NOT NULL)");
-        });
-
-        modelBuilder.Entity<AspNetUsers>(entity =>
-        {
-            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
-                .IsUnique()
-                .HasFilter("([NormalizedUserName] IS NOT NULL)");
-
-            entity.HasMany(d => d.Role).WithMany(p => p.User)
-                .UsingEntity<Dictionary<string, object>>(
-                    "AspNetUserRoles",
-                    r => r.HasOne<AspNetRoles>().WithMany().HasForeignKey("RoleId"),
-                    l => l.HasOne<AspNetUsers>().WithMany().HasForeignKey("UserId"),
-                    j =>
-                    {
-                        j.HasKey("UserId", "RoleId");
-                        j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
-                    });
+            entity.HasOne(d => d.IdEstacionNavigation).WithMany(p => p.Accesos)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_accesos_estaciones");
         });
 
         modelBuilder.Entity<Buses>(entity =>
         {
-            entity.HasKey(e => e.IdBus).HasName("PK__buses__143B662FE44566CA");
+            entity.HasKey(e => e.IdBus).HasName("PK__buses__143B662FEA8F6057");
 
             entity.Property(e => e.Capacidad).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdLinea).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdParqueo).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdLineaNavigation).WithMany(p => p.Buses).HasConstraintName("FK_buses_lineas");
+            entity.HasOne(d => d.IdLineaNavigation).WithMany(p => p.Buses)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_buses_lineas");
 
-            entity.HasOne(d => d.IdParqueoNavigation).WithMany(p => p.Buses).HasConstraintName("FK_buses_parqueos");
+            entity.HasOne(d => d.IdParqueoNavigation).WithMany(p => p.Buses)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_buses_parqueos");
         });
 
         modelBuilder.Entity<Empleados>(entity =>
         {
-            entity.HasKey(e => e.IdEmpleado).HasName("PK__empleado__922CA26925803143");
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__empleado__922CA26947725BB9");
 
             entity.Property(e => e.Direccion).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdPuesto).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Nombre).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Telefono).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdPuestoNavigation).WithMany(p => p.Empleados).HasConstraintName("FK_empleados_puestos");
+            entity.HasOne(d => d.IdPuestoNavigation).WithMany(p => p.Empleados)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_empleados_puestos");
         });
 
         modelBuilder.Entity<EstacionLinea>(entity =>
         {
-            entity.HasKey(e => new { e.IdEstacion, e.IdLinea }).HasName("PK__estacion__24BB16B0EAE0D513");
+            entity.HasKey(e => new { e.IdEstacion, e.IdLinea }).HasName("PK__estacion__24BB16B01A3D3B89");
 
             entity.Property(e => e.DistanciaSiguiente).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Orden).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdEstacionNavigation).WithMany(p => p.EstacionLinea)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_estacion_linea_estaciones");
+            entity.HasOne(d => d.IdEstacionNavigation).WithMany(p => p.EstacionLinea).HasConstraintName("FK_estacion_linea_estaciones");
 
-            entity.HasOne(d => d.IdLineaNavigation).WithMany(p => p.EstacionLinea)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_estacion_linea_lineas");
+            entity.HasOne(d => d.IdLineaNavigation).WithMany(p => p.EstacionLinea).HasConstraintName("FK_estacion_linea_lineas");
         });
 
         modelBuilder.Entity<Estaciones>(entity =>
         {
-            entity.HasKey(e => e.IdEstacion).HasName("PK__estacion__0D552EC89F6C7137");
+            entity.HasKey(e => e.IdEstacion).HasName("PK__estacion__0D552EC836665469");
 
             entity.Property(e => e.Direccion).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdMunicipalidad).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Nombre).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdMunicipalidadNavigation).WithMany(p => p.Estaciones).HasConstraintName("FK_estaciones_municipalidades");
+            entity.HasOne(d => d.IdMunicipalidadNavigation).WithMany(p => p.Estaciones)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_estaciones_municipalidades");
         });
 
         modelBuilder.Entity<Lineas>(entity =>
         {
-            entity.HasKey(e => e.IdLinea).HasName("PK__lineas__9EE387836FCF7494");
+            entity.HasKey(e => e.IdLinea).HasName("PK__lineas__9EE3878360B573B3");
 
             entity.Property(e => e.Longitud).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Nombre).HasDefaultValueSql("(NULL)");
@@ -149,7 +118,7 @@ public partial class TransmetroContext : DbContext
 
         modelBuilder.Entity<Municipalidades>(entity =>
         {
-            entity.HasKey(e => e.IdMunicipalidad).HasName("PK__municipa__06FCE218C607D6EF");
+            entity.HasKey(e => e.IdMunicipalidad).HasName("PK__municipa__06FCE218201AA514");
 
             entity.Property(e => e.Direccion).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Nombre).HasDefaultValueSql("(NULL)");
@@ -157,7 +126,7 @@ public partial class TransmetroContext : DbContext
 
         modelBuilder.Entity<Parqueos>(entity =>
         {
-            entity.HasKey(e => e.IdParqueo).HasName("PK__parqueos__9B329ED04C5E20E3");
+            entity.HasKey(e => e.IdParqueo).HasName("PK__parqueos__9B329ED0FC5FB5F0");
 
             entity.Property(e => e.Capacidad).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Ubicacion).HasDefaultValueSql("(NULL)");
@@ -165,7 +134,7 @@ public partial class TransmetroContext : DbContext
 
         modelBuilder.Entity<Puestos>(entity =>
         {
-            entity.HasKey(e => e.IdPuesto).HasName("PK__puestos__88D8DDB19757FDE1");
+            entity.HasKey(e => e.IdPuesto).HasName("PK__puestos__88D8DDB1DF99D534");
 
             entity.Property(e => e.Descripcion).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.Nombre).HasDefaultValueSql("(NULL)");
@@ -173,16 +142,20 @@ public partial class TransmetroContext : DbContext
 
         modelBuilder.Entity<TurnosBus>(entity =>
         {
-            entity.HasKey(e => e.IdTurno).HasName("PK__turnos_b__F1C3D8730F00137A");
+            entity.HasKey(e => e.IdTurno).HasName("PK__turnos_b__F1C3D873C18144EB");
 
             entity.Property(e => e.FechaFin).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.FechaInicio).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdBus).HasDefaultValueSql("(NULL)");
             entity.Property(e => e.IdEmpleado).HasDefaultValueSql("(NULL)");
 
-            entity.HasOne(d => d.IdBusNavigation).WithMany(p => p.TurnosBus).HasConstraintName("FK_turnos_bus_buses");
+            entity.HasOne(d => d.IdBusNavigation).WithMany(p => p.TurnosBus)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_turnos_bus_buses");
 
-            entity.HasOne(d => d.IdEmpleadoNavigation).WithMany(p => p.TurnosBus).HasConstraintName("FK_turnos_bus_empleados");
+            entity.HasOne(d => d.IdEmpleadoNavigation).WithMany(p => p.TurnosBus)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_turnos_bus_empleados");
         });
 
         OnModelCreatingPartial(modelBuilder);
